533ab1471fe67705def0ab6b1019ab5a99901d2b
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-05-12 19:24:21 +0530

    Remove the database cerdentials DatabaseConfig

--- src/com/flowerdesk/configuration/DatabaseConfig.java
+++ src/com/flowerdesk/configuration/DatabaseConfig.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     private static final String URL="jdbc:mysql://localhost:3306/FlowerDesk";
-    private static final String USER_NAME="";
-    private static final String PASSWORD="";
+    private static final String USER_NAME="root";
+    private static final String PASSWORD="Aadhi@123";
     private static Connection connection=null;
 
     public static Connection getConnection()throws SQLException {
